d99ba27d0b164ea7ad5a6920774ead9d5ae1f051
Teffen Ellis (Eric) <teffen@nirri.us>
2016-05-02 03:18:42 -0700

    Fix numerous issues surrounding positioning.
    - Fix issue where small viewport is not compensated.
    - Fix issue where loading is erratic.
    - Adjust animation to be smoother.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 (function () {
   if (!window.addEventListener) return // Check for IE9+
 
-  const caret = document.createElement("div")
+  const message = document.createElement("eager-message")
+  const caret = document.createElement("eager-caret")
   const TEXT_SHADOWS = {
     dark: "#333333",
     light: "#efefef",
     none: "transparent"
   }
 
-  caret.className = "eager-caret"
   caret.innerHTML = `<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
     <path d="M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z"/>
   </svg>`
 
   let container
-  let message
   let options = INSTALL_OPTIONS
+  let animationFrame
 
   function easeInOutQuad(time, value, delta, duration) {
     time /= duration / 2
@@ -31,6 +31,8 @@
     const delta = finalY - initialY
     const increment = 20
     const start = Date.now()
+    const animate = window.requestAnimationFrame || window.setTimeout
+    const cancelAnimate = window.cancelAnimationFrame || window.clearTimeout
 
     let currentTime = 0
 
@@ -40,55 +42,84 @@
       // This limit serves to prevent an infinite loop if scrolling is interrupted by a user or event handler.
       if (elapsed > duration * 2) {
         element.scrollTop = finalY
-      }
-      else {
-        currentTime += increment
-        element.scrollTop = easeInOutQuad(currentTime, initialY, delta, duration)
+        return
       }
 
-      if (element.scrollTop < finalY) window.setTimeout(animateScroll, increment)
+      currentTime += increment
+      element.scrollTop = easeInOutQuad(currentTime, initialY, delta, duration)
+
+      if (element.scrollTop < finalY) animationFrame = animate(animateScroll, increment)
     }
 
+    cancelAnimate(animationFrame)
     animateScroll()
   }
 
   function centerMessage() {
-    if (!container || !message) return // Elements not ready
+    if (!container) return // Elements not ready
 
     const centerAdjustment = (container.clientHeight - message.clientHeight) / 2
 
     if (centerAdjustment > 0) message.style.transform = `translateY(${centerAdjustment}px)`
   }
 
-  function updateElement() {
-    container = Eager.createElement(options.location, container)
-    container.className = "eager-hero-image"
+  function updateInnerContent() {
+    if (!container) return
+
     container.setAttribute("data-alignment", options.alignment)
 
-    container.style.backgroundImage = options.backgroundImage ? `url("${options.backgroundImage}")` : ""
     container.style.color = options.textColor
     container.style.textShadow = `1px 1px 3px ${TEXT_SHADOWS[options.textShadowColor]}`
+    message.innerHTML = options.message.html
 
     caret.firstChild.style.fill = options.textColor
+  }
 
+  function updateElement() {
+    const {backgroundImage} = options
     const parent = document.querySelector(options.location.selector)
-    const initialPaddingTop = parent.style.paddingTop || "0px"
 
-    parent.style.paddingTop = `calc(100vh + ${initialPaddingTop})`
-
-    message = document.createElement("eager-message")
+    container = Eager.createElement(options.location, container)
+    container.className = "eager-hero-image"
 
     container.addEventListener("click", () => scrollToTop({
       element: parent,
       finalY: container.clientHeight
     }))
 
-    message.innerHTML = options.message.html
+    updateInnerContent()
 
     container.appendChild(message)
     container.appendChild(caret)
 
+    const initialPaddingTop = parent.style.paddingTop || "0px"
+    const initialPaddingBottom = parent.style.paddingBottom || "0px"
+    let viewportCompensation = 0
+
+    if (document.body.clientHeight < document.documentElement.clientHeight) {
+      viewportCompensation = document.documentElement.clientHeight - document.body.clientHeight
+    }
+
+    parent.style.paddingBottom = `calc(${initialPaddingBottom} + ${viewportCompensation}px)`
+    parent.style.paddingTop = `calc(100vh + ${initialPaddingTop})`
+
     centerMessage()
+
+    const prefetchImage = document.createElement("img")
+
+    function onComplete() {
+      container.setAttribute("data-state", "loaded")
+    }
+
+    if (!backgroundImage) return onComplete()
+
+    prefetchImage.onload = () => {
+      container.style.backgroundImage = `url("${backgroundImage}")`
+      onComplete()
+    }
+
+    prefetchImage.onerror = onComplete
+    prefetchImage.src = backgroundImage
   }
 
   function onReady() {
@@ -97,10 +128,16 @@
   }
 
   window.INSTALL_SCOPE = {
-    setOptions(nextOptions) {
+    updateElement(nextOptions) {
       options = nextOptions
 
       updateElement()
+    },
+    updateInnerContent(nextOptions) {
+      options = nextOptions
+
+      updateInnerContent()
+      centerMessage()
     }
   }
 
--- build/app.js
+++ build/app.js
@@ -1,16 +1,21 @@
 "use strict";
 
 (function () {
-  var BACKGROUND_IMAGE = "https://eager-app-images.imgix.net/lturyv6bQ0KLnXdnNLCi_life.jpg";
-  var CONTAINER_CLASS = "eager-hero-image";
-  var caret = document.createElement("div");
+  if (!window.addEventListener) return; // Check for IE9+
 
-  caret.classList.add("eager-caret");
+  var message = document.createElement("eager-message");
+  var caret = document.createElement("eager-caret");
+  var TEXT_SHADOWS = {
+    dark: "#333333",
+    light: "#efefef",
+    none: "transparent"
+  };
+
   caret.innerHTML = "<svg width=\"1792\" height=\"1792\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\">\n    <path d=\"M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z\"/>\n  </svg>";
 
   var container = void 0;
-  var message = void 0;
   var options = INSTALL_OPTIONS;
+  var animationFrame = void 0;
 
   function easeInOutQuad(time, value, delta, duration) {
     time /= duration / 2;
@@ -21,17 +26,19 @@
     return -delta / 2 * (time * (time - 2) - 1) + value;
   }
 
-  function scrollToTop(options) {
-    var element = options.element;
-    var _options$duration = options.duration;
-    var duration = _options$duration === undefined ? 600 : _options$duration;
-    var _options$finalY = options.finalY;
-    var finalY = _options$finalY === undefined ? 0 : _options$finalY;
+  function scrollToTop(_ref) {
+    var element = _ref.element;
+    var _ref$duration = _ref.duration;
+    var duration = _ref$duration === undefined ? 600 : _ref$duration;
+    var _ref$finalY = _ref.finalY;
+    var finalY = _ref$finalY === undefined ? 0 : _ref$finalY;
 
     var initialY = element.scrollTop;
     var delta = finalY - initialY;
     var increment = 20;
     var start = Date.now();
+    var animate = window.requestAnimationFrame || window.setTimeout;
+    var cancelAnimate = window.cancelAnimationFrame || window.clearTimeout;
 
     var currentTime = 0;
 
@@ -41,44 +48,47 @@
       // This limit serves to prevent an infinite loop if scrolling is interrupted by a user or event handler.
       if (elapsed > duration * 2) {
         element.scrollTop = finalY;
-      } else {
-        currentTime += increment;
-        element.scrollTop = easeInOutQuad(currentTime, initialY, delta, duration);
+        return;
       }
 
-      if (element.scrollTop < finalY) setTimeout(animateScroll, increment);
+      currentTime += increment;
+      element.scrollTop = easeInOutQuad(currentTime, initialY, delta, duration);
+
+      if (element.scrollTop < finalY) animationFrame = animate(animateScroll, increment);
     }
 
+    cancelAnimate(animationFrame);
     animateScroll();
   }
 
   function centerMessage() {
-    if (!container || !message) return; // Elements not ready
+    if (!container) return; // Elements not ready
 
     var centerAdjustment = (container.clientHeight - message.clientHeight) / 2;
 
     if (centerAdjustment > 0) message.style.transform = "translateY(" + centerAdjustment + "px)";
   }
 
-  function updateElement() {
-    container = Eager.createElement(options.location, container);
-    container.classList.add(CONTAINER_CLASS);
+  function _updateInnerContent() {
+    if (!container) return;
+
     container.setAttribute("data-alignment", options.alignment);
 
-    Object.assign(container.style, {
-      backgroundImage: "url(" + (options.backgroundImage || BACKGROUND_IMAGE) + ")",
-      color: options.textColor,
-      textShadow: options.textShadowColor ? "1px 1px 3px " + options.textShadowColor : ""
-    });
+    container.style.color = options.textColor;
+    container.style.textShadow = "1px 1px 3px " + TEXT_SHADOWS[options.textShadowColor];
+    message.innerHTML = options.message.html;
 
     caret.firstChild.style.fill = options.textColor;
+  }
+
+  function _updateElement() {
+    var _options = options;
+    var backgroundImage = _options.backgroundImage;
 
     var parent = document.querySelector(options.location.selector);
-    var initialPaddingTop = parent.style.paddingTop || "0px";
 
-    parent.style.paddingTop = "calc(100vh + " + initialPaddingTop + ")";
-
-    message = document.createElement("eager-message");
+    container = Eager.createElement(options.location, container);
+    container.className = "eager-hero-image";
 
     container.addEventListener("click", function () {
       return scrollToTop({
@@ -87,24 +97,57 @@
       });
     });
 
-    message.innerHTML = options.message.html;
+    _updateInnerContent();
 
     container.appendChild(message);
     container.appendChild(caret);
 
+    var initialPaddingTop = parent.style.paddingTop || "0px";
+    var initialPaddingBottom = parent.style.paddingBottom || "0px";
+    var viewportCompensation = 0;
+
+    if (document.body.clientHeight < document.documentElement.clientHeight) {
+      viewportCompensation = document.documentElement.clientHeight - document.body.clientHeight;
+    }
+
+    parent.style.paddingBottom = "calc(" + initialPaddingBottom + " + " + viewportCompensation + "px)";
+    parent.style.paddingTop = "calc(100vh + " + initialPaddingTop + ")";
+
     centerMessage();
+
+    var prefetchImage = document.createElement("img");
+
+    function onComplete() {
+      container.setAttribute("data-state", "loaded");
+    }
+
+    if (!backgroundImage) return onComplete();
+
+    prefetchImage.onload = function () {
+      container.style.backgroundImage = "url(\"" + backgroundImage + "\")";
+      onComplete();
+    };
+
+    prefetchImage.onerror = onComplete;
+    prefetchImage.src = backgroundImage;
   }
 
   function onReady() {
-    updateElement();
+    _updateElement();
     window.addEventListener("resize", centerMessage);
   }
 
-  INSTALL_SCOPE = {
-    setOptions: function setOptions(nextOptions) {
+  window.INSTALL_SCOPE = {
+    updateElement: function updateElement(nextOptions) {
       options = nextOptions;
 
-      updateElement();
+      _updateElement();
+    },
+    updateInnerContent: function updateInnerContent(nextOptions) {
+      options = nextOptions;
+
+      _updateInnerContent();
+      centerMessage();
     }
   };
 
